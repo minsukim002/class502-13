1c0b673921115ce59de7b58756d52390a92b0a8b
minsukim002 <elancla14@gmail.com>
2024-04-12 16:20:41 +0900

    변수와 자료형 실습

---
+++ 응용 SW 기초 기술활용/source/day01_2/src/exam02/exam01.java
@@ -0,0 +1,11 @@
+package exam02;
+
+public class exam01 {
+    public static void main (String [] args){
+
+    int num1= 10000000;
+    byte num2 = 55;
+
+
+    }
+}
---
+++ 응용 SW 기초 기술활용/source/day01_2/src/exam03/exam01.java
@@ -0,0 +1,20 @@
+package exam03;
+
+import java.net.SocketOption;
+import java.sql.SQLOutput;
+
+public class exam01 {
+    public static void main(String[] args) {
+       int num1 = 13;
+       double num2 = 3.6;
+
+        int result =(int) (num1 * num2);
+        System.out.println(result);
+   }
+}
+
+
+
+
+
+
